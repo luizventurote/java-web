853998e4f36b1dc3df5dae7831f79a862f2f4891
luizventurote <luiz_17@ymail.com>
2015-05-20 00:30:52 -0300

    CRUD de diretor implementado

--- src/java/Application/Apl_Diretor.java
+++ src/java/Application/Apl_Diretor.java
@@ -1,12 +1,38 @@
 package Application;
 
+import static Application.Apl_Default.RESULT_ERRO;
+import static Application.Apl_Default.RESULT_OK;
+import static Application.Apl_Default.openSession;
+
+import Util.FormBuild;
 import Model.Diretor;
+import java.sql.SQLException;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 
 /**
  * @author Luiz Venturote
  */
 public class Apl_Diretor extends Apl_Default {
     
+    
+    /**
+    * Class main name base
+    */
+    private static String classMainName = "Diretor";
+
+    
+    /**
+    * Constructor
+    */
+    public Apl_Diretor() {
+        this.setMainName(classMainName);
+    }
+    
+    
+    /**
+    * Incluir
+    */
     public static int incluir(String nome){
 	
         if(nome.equals(""))
@@ -17,6 +43,139 @@ public class Apl_Diretor extends Apl_Default {
 	salvar(d);
 		
 	return RESULT_OK;
+    }
+    
+    
+    /**
+     * Atualizar
+     */
+    public static int update(Diretor obj) throws Exception, SQLException, HibernateException {
+        
+        // Open Session
+        Session session = openSession();
+
+        try {
+
+            session.beginTransaction();
+            session.update(obj);
+            session.getTransaction().commit();
+
+        } catch (HibernateException he) {
+            session.getTransaction().rollback();
+            return RESULT_ERRO;
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        
+        return RESULT_OK;
+        
+    }    
+    
+    
+    /**
+     * Deletar
+     */
+    public static int deletar(String tabela, int id) throws Exception, SQLException, HibernateException {
+        
+        // Get object
+        Diretor obj = (Diretor) getRegistro(tabela, id);
+        
+        // Open Session
+        Session session = openSession();;
+
+        try {
+
+            // Delect object
+            session.beginTransaction();
+            session.delete(obj);
+            session.getTransaction().commit();
+
+        } catch (HibernateException he) {
+            session.getTransaction().rollback();
+            return RESULT_ERRO;
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return RESULT_OK;
+    }
+    
+    
+    /**
+    * Retorna a linha de exibição do list
+    */
+    public static String getFormListHeader() {
+        
+        String form =   "<tr>"+
+                            "<th>ID</th>\n" +
+                            "<th>Nome</th>\n" +
+                            "<th>Ações</th>"+
+                        "</tr>";
+        
+        return form;
+        
+    }
+    
+    
+    /**
+    * Retorna a linha de exibição do list
+    */
+    public static String getFormList(Object obj) {
+        
+        Diretor o = (Diretor) obj;
+        
+        String form =   "<tr>"+
+                            "<th scope='row'>" + o.getId()+ "</th>" + 
+                            "<td>" + o.getNome()+ "</td>"+
+                            "<td>"+Apl_Default.getFormActions(classMainName, o.getId())+"</td>"+
+                        "</tr>";
+        
+        return form;
+        
+    }
+    
+    
+    /**
+    * Retorna O formulário da classe
+    */
+    public static String getForm() {
+        
+        // Form
+        String form = "";
+        
+        // Form builder
+        FormBuild buildForm = new FormBuild();
+        
+        // Name
+        form = form + buildForm.getInputText("Nome", "nome");
+            
+        return form;
+        
+    }
+    
+    
+    /**
+    * Retorna O formulário de atualização da classe com os dados do objeto
+    */
+    public static String getFormToUpdate(int id) {
+        
+        // Main Object
+        Diretor o = (Diretor) Apl_Default.getRegistro(classMainName, id);
+        
+        // Form
+        String form = "";
+        
+        // Form builder
+        FormBuild buildForm = new FormBuild();
+        
+        // Name
+        form = form + buildForm.getInputText("Nome", "nome", o.getNome());
+            
+        return form;
+        
     }
     
 }
--- src/java/Controller/Servlet_Diretor.java
+++ src/java/Controller/Servlet_Diretor.java
@@ -1,6 +1,7 @@
 package Controller;
 
 import Application.Apl_Default;
+import Model.Diretor;
 import java.io.IOException;
 import java.util.List;
 import javax.servlet.ServletException;
@@ -15,6 +16,8 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "Servlet_Diretor", urlPatterns = {"/Diretor"})
 public class Servlet_Diretor extends HttpServlet {
+    
+    private static String tabela = "Diretor";
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -25,21 +28,63 @@ public class Servlet_Diretor extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         
-        String acao = request.getParameter("acao");
+        String url = "List.jsp?list_type="+tabela;
         
+        // Verifica a ação do usuário informada na URL
+        String acao = request.getParameter("acao");        
         if(acao != null ) {
             
+            // Cadastrar
             if(acao.equals("cad")){
+                
                 String nome = request.getParameter("nome");
-                if(Application.Apl_Diretor.incluir(nome) == Application.Apl_Diretor.RESULT_OK){}
+                
+                if(Application.Apl_Diretor.incluir(nome) == Application.Apl_Default.RESULT_OK){
+                    url = url + "&erro=0";
+                } else {
+                    url = url + "&erro=1";
+                }
+                
             }
             
+            // Update
+            if(acao.equals("upd")){
+                
+                // Object data
+                int id      = Integer.parseInt(request.getParameter("id"));
+                String nome = request.getParameter("nome");
+                
+                // Main Object
+                Diretor obj = (Diretor) Apl_Default.getRegistro(tabela, id);
+                
+                // Set object values
+                obj.setNome(nome);
+                
+                try {
+                    if(Application.Apl_Diretor.update(obj) == Application.Apl_Default.RESULT_OK){}
+                } catch (Exception ex) {}
+                
+            }
+            
+            // Deletar
+            if(acao.equals("del")){
+                
+                int id = Integer.parseInt(request.getParameter("id"));
+                
+                try {
+                    if(Application.Apl_Diretor.deletar(tabela, id) == Application.Apl_Default.RESULT_OK){
+                        url = url + "&erro=0";
+                    } else {
+                        url = url + "&erro=1";
+                    }
+                } catch (Exception ex) {}
+                
+            }   
         }
 		
-        request.getRequestDispatcher("ListDiretor.jsp").forward(request,response); 
+        response.sendRedirect(url); 
         
     }
     
@@ -48,7 +93,7 @@ public class Servlet_Diretor extends HttpServlet {
      * @return List Lista de registros
      */
     public static List consultarTodosRegistros() {
-        return Apl_Default.consultarTodosRegistros("Diretor");
+        return Apl_Default.consultarTodosRegistros(tabela);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">

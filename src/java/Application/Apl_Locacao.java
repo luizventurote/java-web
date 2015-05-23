c70d9c5430f7e93b83391f5c46a3eb8d32041f7a
luizventurote <luiz_17@ymail.com>
2015-05-23 14:10:30 -0300

    CRUD de locacao adicionado

--- src/java/Application/Apl_Locacao.java
+++ src/java/Application/Apl_Locacao.java
@@ -1,14 +1,47 @@
 package Application;
 
+import static Application.Apl_Default.RESULT_ERRO;
+import static Application.Apl_Default.RESULT_OK;
+import static Application.Apl_Default.getRegistro;
+import static Application.Apl_Default.openSession;
+
+import Controller.Servlet_Cliente;
+import Controller.Servlet_Item;
+import Controller.Servlet_Titulo;
 import Model.Cliente;
 import Model.Item;
 import Model.Locacao;
+import Model.Titulo;
+import Util.FormBuild;
+import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 
 /**
  * @author Luiz Venturote
  */
 public class Apl_Locacao extends Apl_Default {
     
+    
+    /**
+    * Class main name base
+    */
+    private static String classMainName = "Locacao";
+
+    
+    /**
+    * Constructor
+    */
+    public Apl_Locacao() {
+        this.setMainName(classMainName);
+    }
+    
+    
+    /**
+    * Incluir
+    */
     public static int incluir(String dataLocacao, String dataDevolucaoPrevista, double valorCobrado, Cliente cliente, Item item){
 		
 	Locacao l = new Locacao(dataLocacao, dataDevolucaoPrevista, valorCobrado, cliente, item);
@@ -16,6 +49,237 @@ public class Apl_Locacao extends Apl_Default {
 	salvar(l);
 		
         return RESULT_OK;
+    }
+    
+    
+    /**
+     * Atualizar
+     */
+    public static int update(Locacao obj) throws Exception, SQLException, HibernateException {
+        
+        // Open Session
+        Session session = openSession();
+
+        try {
+
+            session.beginTransaction();
+            session.update(obj);
+            session.getTransaction().commit();
+
+        } catch (HibernateException he) {
+            session.getTransaction().rollback();
+            return RESULT_ERRO;
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        
+        return RESULT_OK;
+        
+    }
+    
+    
+    /**
+     * Deletar
+     */
+    public static int deletar(String tabela, int id) throws Exception, SQLException, HibernateException {
+        
+        // Get object
+        Locacao obj = (Locacao) getRegistro(tabela, id);
+        
+        // Open Session
+        Session session = openSession();
+
+        try {
+
+            // Delect object
+            session.beginTransaction();
+            session.delete(obj);
+            session.getTransaction().commit();
+
+        } catch (HibernateException he) {
+            session.getTransaction().rollback();
+            return RESULT_ERRO;
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return RESULT_OK;
+    }
+    
+    
+    /**
+    * Retorna a linha de exibição do list
+    */
+    public static String getFormListHeader() {
+        
+        String form =   "<tr>"+
+                            "<th>id</th>\n" +
+                            "<th>Cliente</th>\n" +
+                            "<th>Item / Título</th>\n" +
+                            "<th>Data de locação</th>\n" +
+                            "<th>Data de devolução</th>\n" +
+                            "<th>Valor</th>\n" +
+                            "<th>Ações</th>"+
+                        "</tr>";
+        
+        return form;
+        
+    }
+    
+    
+    /**
+    * Retorna a linha de exibição do list
+    */
+    public static String getFormList(Object obj) {
+        
+        Locacao o = (Locacao) obj;
+        
+        String form =   "<tr>"+
+                            "<th scope='row'>" + o.getId() + "</th>" + 
+                            "<td>" + o.getCliente().getNome() + "</td>"+
+                            "<td>" + o.getItem().getTitulo().getNome() + "</td>"+
+                            "<td>" + o.getDataLocacao() + "</td>"+
+                            "<td>" + o.getDataDevolucao() + "</td>"+
+                            "<td>" + o.getValorCobrado() + "</td>"+
+                            "<td>"+Apl_Default.getFormActions(classMainName, o.getId())+"</td>"+
+                        "</tr>";
+        
+        return form;
+        
+    }
+    
+    
+    /**
+    * Retorna O formulário da classe
+    */
+    public static String getForm() {
+        
+        // Form
+        String form = "";
+        
+        // Form builder
+        FormBuild buildForm = new FormBuild();
+        
+        // Registros
+        List registros;
+        
+        // Data de locação
+        form = form + buildForm.getInputText("Data de locação", "data_locacao");
+        
+        // Data
+        form = form + buildForm.getInputText("Data de devolução", "data_devolucao");
+        
+        // Data
+        form = form + buildForm.getInputText("Valor", "valor");  
+        
+        // Select Cliente
+        form = form +"<div class='form-group'>\n" +
+                        "<label for='exampleInput'>Cliente</label>\n" +
+                        "<select class='form-control' name='cliente'>";
+        
+        registros = Servlet_Cliente.consultarTodosRegistros();
+        Iterator<Cliente> cli = registros.iterator();
+                            
+        while (cli.hasNext()){			
+            Cliente o = (Cliente)cli.next();
+            form = form + "<option value='"+o.getNumero_inscricao()+"'>"+o.getNome()+"</option>";								
+        }
+        
+        form = form +"</select></div>";
+        
+        // Select Item
+        form = form +"<div class='form-group'>\n" +
+                        "<label for='exampleInput'>Item / Título</label>\n" +
+                        "<select class='form-control' name='item'>";
+        
+        registros = Servlet_Item.consultarTodosRegistros();
+        Iterator<Item> item = registros.iterator();
+                            
+        while (item.hasNext()){			
+            Item o = (Item)item.next();
+            form = form + "<option value='"+o.getId()+"'>"+o.getTitulo().getNome()+"</option>";								
+        }
+        
+        form = form +"</select></div>";
+            
+        return form;
+        
+    }
+    
+    
+    /**
+    * Retorna O formulário de atualização da classe com os dados do objeto
+    */
+    public static String getFormToUpdate(int id) {
+        
+        // Main Object
+        Locacao main_obj = (Locacao) Apl_Default.getRegistro(classMainName, id);
+        
+        // Form
+        String form = "";
+        
+        // Form builder
+        FormBuild buildForm = new FormBuild();
+        
+        // Registros
+        List registros;
+        
+        // Data de locação
+        form = form + buildForm.getInputText("Data de locação", "data_locacao", main_obj.getDataLocacao());
+        
+        // Data
+        form = form + buildForm.getInputText("Data de devolução", "data_devolucao", main_obj.getDataDevolucao());
+        
+        // Data
+        form = form + buildForm.getInputText("Valor", "valor", Double.toString(main_obj.getValorCobrado()));  
+        
+        // Select Cliente
+        form = form +"<div class='form-group'>\n" +
+                        "<label for='exampleInput'>Cliente</label>\n" +
+                        "<select class='form-control' name='cliente'>";
+        
+        registros = Servlet_Cliente.consultarTodosRegistros();
+        Iterator<Cliente> cli = registros.iterator();
+                            
+        while (cli.hasNext()){			
+            Cliente o = (Cliente)cli.next();
+            
+            if(main_obj.getCliente().getNumero_inscricao() == o.getNumero_inscricao()) {
+                form = form + "<option value='"+o.getNumero_inscricao()+"' selected>"+o.getNome()+"</option>";
+            } else {
+                form = form + "<option value='"+o.getNumero_inscricao()+"'>"+o.getNome()+"</option>";
+            }
+            								
+        }
+        
+        form = form +"</select></div>";
+        
+        // Select Item
+        form = form +"<div class='form-group'>\n" +
+                        "<label for='exampleInput'>Item / Titulo</label>\n" +
+                        "<select class='form-control' name='item'>";
+        
+        registros = Servlet_Item.consultarTodosRegistros();
+        Iterator<Item> item = registros.iterator();
+                            
+        while (item.hasNext()){			
+            Item o = (Item)item.next();
+            
+            if(main_obj.getItem().getId() == o.getId()) {
+                form = form + "<option value='"+o.getId()+"' selected>"+o.getTitulo().getNome()+"</option>";
+            } else {
+                form = form + "<option value='"+o.getId()+"'>"+o.getTitulo().getNome()+"</option>";
+            }
+            								
+        }
+        
+        form = form +"</select></div>";
+            
+        return form;
+        
     }
    
 }
--- src/java/Controller/Servlet_Locacao.java
+++ src/java/Controller/Servlet_Locacao.java
@@ -3,6 +3,8 @@ package Controller;
 import Application.Apl_Default;
 import Model.Cliente;
 import Model.Item;
+import Model.Locacao;
+import Model.Titulo;
 import java.io.IOException;
 import java.util.List;
 import javax.servlet.ServletException;
@@ -17,6 +19,8 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "Servlet_Locacao", urlPatterns = {"/Locacao"})
 public class Servlet_Locacao extends HttpServlet {
+    
+    private static String tabela = "Locacao";
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -29,31 +33,83 @@ public class Servlet_Locacao extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         
-        String acao = request.getParameter("acao");
+        String url = "List.jsp?list_type="+tabela;
         
-        if(acao != null ) {
+        // Verifica a ação do usuário informada na URL
+        String acao = request.getParameter("acao");
+        if (acao != null) {
             
+            // Cadastrar
             if(acao.equals("cad")){
                 
-                int id_cliente = Integer.parseInt( request.getParameter("id_cliente") );
-                int id_item = Integer.parseInt( request.getParameter("id_item") );
-                String data_locacao = request.getParameter("data_locacao");
-                String data_devolucao = request.getParameter("data_devolucao");
-                double valor = Double.parseDouble( request.getParameter("valor") );
+                int cliente             = Integer.parseInt( request.getParameter("cliente") );
+                int item                = Integer.parseInt( request.getParameter("item") );
+                String data_locacao     = request.getParameter("data_locacao");
+                String data_devolucao   = request.getParameter("data_devolucao");
+                double valor            = Double.parseDouble( request.getParameter("valor") );
                 
-                /**
-                 * Recupera registros
-                 */
-                Cliente cli = (Cliente) Apl_Default.getRegistro("Cliente", id_cliente);
-                Item item = (Item) Apl_Default.getRegistro("Item", id_item);
+                //Recupera registros
+                Cliente cli = (Cliente) Apl_Default.getRegistro("Cliente", cliente);
+                Item it     = (Item) Apl_Default.getRegistro("Item", item);
                 
-                if(Application.Apl_Locacao.incluir(data_locacao, data_devolucao, valor, cli, item) == Application.Apl_Ator.RESULT_OK){}
+                 if(Application.Apl_Locacao.incluir(data_locacao, data_devolucao, valor, cli, it) == Application.Apl_Ator.RESULT_OK){
+                    url = url + "&erro=0";
+                } else {
+                    url = url + "&erro=1";
+                }
+                
+            }            
+            
+            // Update
+            if(acao.equals("upd")){
+                
+                // Object data
+                int id          = Integer.parseInt(request.getParameter("id"));
+                int cliente             = Integer.parseInt( request.getParameter("cliente") );
+                int item                = Integer.parseInt( request.getParameter("item") );
+                String data_locacao     = request.getParameter("data_locacao");
+                String data_devolucao   = request.getParameter("data_devolucao");
+                double valor            = Double.parseDouble( request.getParameter("valor") );
+                
+                //Recupera registros
+                Cliente cli = (Cliente) Apl_Default.getRegistro("Cliente", cliente);
+                Item it  = (Item) Apl_Default.getRegistro("Item", item);
+                
+                // Main Object
+                Locacao obj = (Locacao) Apl_Default.getRegistro(tabela, id);
+                
+                // Set object values
+                obj.setCliente(cli);
+                obj.setItem(it);
+                obj.setDataDevolucao(data_devolucao);
+                obj.setDataLocacao(data_locacao);
+                obj.setValorCobrado(valor);
+                
+                try {
+                    if(Application.Apl_Locacao.update(obj) == Application.Apl_Default.RESULT_OK){}
+                } catch (Exception ex) {}
+                
             }
             
+            // Deletar
+            if(acao.equals("del")){
+                
+                int id = Integer.parseInt(request.getParameter("id"));
+                
+                try {
+                    if(Application.Apl_Locacao.deletar(tabela, id) == Application.Apl_Default.RESULT_OK){
+                        url = url + "&erro=0";
+                    } else {
+                        url = url + "&erro=1";
+                    }
+                } catch (Exception ex) {}
+                
+            }
+
         }
         
-        request.getRequestDispatcher("ListLocacao.jsp").forward(request,response);
-        
+        response.sendRedirect(url);
+          
     }
     
     
@@ -62,7 +118,7 @@ public class Servlet_Locacao extends HttpServlet {
      * @return List Lista de registros
      */
     public static List consultarTodosRegistros() {
-        return Apl_Default.consultarTodosRegistros("Locacao");
+        return Apl_Default.consultarTodosRegistros(tabela);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
